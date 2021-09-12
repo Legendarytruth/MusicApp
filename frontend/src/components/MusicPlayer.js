95a43ba615d7a2e60cce7673ea588cd22312f506
Legendarytruth <arthur.sit99@gmail.com>
2021-09-11 21:29:01 -0400

    Added Play, Pause, and Skip functionality

--- frontend/src/components/MusicPlayer.js
+++ frontend/src/components/MusicPlayer.js
@@ -9,7 +9,30 @@ export default class MusicPlayer extends Component {
     constructor(props) {
       super(props);
     }
+
+    skipSong() {
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+      };
+      fetch("/spotify/skip", requestOptions);
+    }
+
+    pauseSong() {
+      const requestOptions = {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+      };
+      fetch("/spotify/pause", requestOptions);
+    }
   
+    playSong() {
+      const requestOptions = {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+      };
+      fetch("/spotify/play", requestOptions);
+    }
     render() {
       const songProgress = (this.props.time / this.props.duration) * 100;
   
@@ -27,13 +50,18 @@ export default class MusicPlayer extends Component {
                 {this.props.artist}
               </Typography>
               <div>
-                <IconButton>
-                  {this.props.is_playing ? <PauseIcon /> : <PlayArrowIcon />}
-                </IconButton>
-                <IconButton>
-                  <SkipNextIcon />
-                </IconButton>
-              </div>
+              <IconButton
+                onClick={() => {
+                  this.props.is_playing ? this.pauseSong() : this.playSong();
+                }}
+              >
+                {this.props.is_playing ? <PauseIcon /> : <PlayArrowIcon />}
+              </IconButton>
+              <IconButton onClick={() => this.skipSong()}>
+                {this.props.votes} / {this.props.votes_required}
+                <SkipNextIcon />
+              </IconButton>
+            </div>
             </Grid>
           </Grid>
           <LinearProgress variant="determinate" value={songProgress} />
